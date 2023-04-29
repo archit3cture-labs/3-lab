d7a206596137ee49dd825485f00f0db7d3125cd1
kreslavskiy <mishak1000236@gmail.com>
2023-04-29 15:34:38 +0300

    Add reaction on mouse clicks

--- ui/window.go
+++ ui/window.go
@@ -27,6 +27,7 @@ type Visualizer struct {
 
 	sz  size.Event
 	pos image.Rectangle
+	mousePos image.Point
 }
 
 func (pw *Visualizer) Main() {
@@ -34,6 +35,7 @@ func (pw *Visualizer) Main() {
 	pw.done = make(chan struct{})
 	pw.pos.Max.X = 200
 	pw.pos.Max.Y = 200
+	pw.mousePos = image.Point{X: 400, Y: 400}
 	driver.Main(pw.run)
 }
 
@@ -118,6 +120,14 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 	case mouse.Event:
 		if t == nil {
 			// TODO: Реалізувати реакцію на натискання кнопки миші.
+			if e.Button == mouse.ButtonLeft && e.Direction == mouse.DirPress {
+				pw.mousePos = image.Point{
+					X: int(e.X),
+					Y: int(e.Y),
+				}
+
+				pw.w.Send(paint.Event{})
+			}
 		}
 
 	case paint.Event:
@@ -134,15 +144,15 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 
 func (pw *Visualizer) drawDefaultUI() {
 	pw.w.Fill(pw.sz.Bounds(), color.White, draw.Src) // Фон.
-
+	x, y := pw.mousePos.X, pw.mousePos.Y
 	// TODO: Змінити колір фону та додати відображення фігури у вашому варіанті.
 	pw.w.Fill(
-		image.Rect(250, 300, 550, 400), 
+		image.Rect(x-150, y-100, x+150, y), 
 		color.RGBA{R: 255, G: 255, B: 0, A: 1}, 
 		draw.Src)
 
 	pw.w.Fill(
-		image.Rect(350, 300, 450, 500), 
+		image.Rect(x-50, y, x+50, y+100), 
 		color.RGBA{R: 255, G: 255, B: 0, A: 1}, 
 		draw.Src)
 	// Малювання білої рамки.
